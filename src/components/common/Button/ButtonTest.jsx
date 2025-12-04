4a74894ec02fc5927dbaa0bb4de652bbea5f9433
RasmusLeddyr <rasmusleddyr@hotmail.com>
2025-12-04 13:37:25 +0100

    button function example

--- src/components/common/Button/ButtonTest.jsx
+++ src/components/common/Button/ButtonTest.jsx
@@ -1,12 +1,39 @@
-import React from "react";
-import styles from "./Button.module.css";
+import { useNavigate } from "react-router-dom";
 import { cx } from "../../../functions/setStyles.jsx";
+import styles from "./Button.module.css";
 
-const Button = (mStyles, gStyles) => {
+const Button = (mStyles, gStyles, content, link, func) => {
+  const navigate = useNavigate();
+  const clickEvent = () => {
+    // If link was given, use navigate for "/" links, or window open otherwise.
+    if (link) {
+      if (link[0] == "/") {
+        navigate(link);
+      } else {
+        window.open(link, "_blank");
+      }
+      return;
+    }
+    // If no link was given, check for function and run it if given.
+    if (typeof func === "function") {
+      func();
+      return;
+    }
+    // If neither link nor function was given, give console warning.
+    console.warn("Button has no link or function.");
+  };
+
   return (
-    <div className={cx(styles, `${mStyles} container active highlight`, `${gStyles}`)}>
-      Button
-    </div>
+    <button
+      className={cx(
+        styles,
+        `${mStyles} container active highlight`,
+        `${gStyles}`
+      )}
+      onClick={clickEvent}
+    >
+      {content}
+    </button>
   );
 };
 
